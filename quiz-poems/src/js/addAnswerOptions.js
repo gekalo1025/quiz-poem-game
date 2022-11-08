8799618eeb3c60f0bb48c24b5536d2444ca5e7d1
Roman Lysiuk <gekalo1025@gmail.com>
2022-11-08 16:36:14 +0200

    feat: add "addAnswerOptions" function

---
+++ quiz-poems/src/js/addAnswerOptions.js
@@ -0,0 +1,32 @@
+import getRandomIntInclusive from "./getRandomIntInclusive";
+import { currentQuestion } from "../pages/quiz-poems/quiz-poems";
+import isCorrectAnswerInList from "./isСorrectAnswerInList";
+
+const answersOptions = document.querySelector(".answers__options");
+
+export default function addAnswersOptions(arrAnswers, quantity) {
+  const correctAuthor = currentQuestion.nameAuthor;
+  answersOptions.innerHTML = "";
+
+  for (let i = 0; i < quantity; i++) {
+    const newAnswer = document.createElement("div");
+    const span = document.createElement("span");
+
+    newAnswer.classList.add("answer");
+
+    newAnswer.textContent = arrAnswers.splice(
+      getRandomIntInclusive(0, arrAnswers.length - 1),
+      1
+    )[0];
+
+    newAnswer.appendChild(span);
+    answersOptions.appendChild(newAnswer);
+  }
+
+  const allAnswer = answersOptions.children;
+
+  if (!isCorrectAnswerInList(allAnswer, correctAuthor)) {
+    allAnswer[getRandomIntInclusive(0, allAnswer.length - 1)].textContent =
+      correctAuthor;
+  }
+}
